27ce951776a0a1af8df7dd4a622ebb76c5fe4d26
Arisha <79106539+Arina842@users.noreply.github.com>
2025-07-10 10:58:06 +0300

    полный вывод

--- print_numpy_array.py
+++ print_numpy_array.py
@@ -1,3 +1,12 @@
 
 a=np.array([1,3,4,5,6,7,8])
+
+# вывод обычного массива
+print('a=[')
 print(', '.join(map(str, a)))
+print(']')
+
+# вывод numpy массива
+print('a=np.array([')
+print(', '.join(map(str, a)))
+print('])')
